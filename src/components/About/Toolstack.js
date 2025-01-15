c140a7514514122f9564c3a89b185d1c3b4322c7
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-01-15 16:42:33 +0530

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -8,24 +8,38 @@ import {
   SiMacos,
 } from "react-icons/si";
 
+// function Toolstack() {
+//   return (
+//     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+//       <Col xs={4} md={2} className="tech-icons">
+//         <SiMacos />
+//       </Col>
+//       <Col xs={4} md={2} className="tech-icons">
+//         <SiVisualstudiocode />
+//       </Col>
+//       <Col xs={4} md={2} className="tech-icons">
+//         <SiPostman />
+//       </Col>
+//       <Col xs={4} md={2} className="tech-icons">
+//         <SiSlack />
+//       </Col>
+//       <Col xs={4} md={2} className="tech-icons">
+//         <SiVercel />
+//       </Col>
+//     </Row>
+//   );
+// }
+
 function Toolstack() {
+  const tools = ["macOS", "Visual Studio Code", "Postman", "Slack", "Vercel"];
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
+      {tools.map((tool, index) => (
+        <Col key={index} xs={4} md={2} className="tech-text">
+          {tool}
+        </Col>
+      ))}
     </Row>
   );
 }
