edae875dfc7fea3b705167e6ee10e706e03e8225
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-01-15 16:40:37 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/Ayushdubey86/portfoliou"
                 target="_blank"
                 className="fork-btn-inner"
               >
